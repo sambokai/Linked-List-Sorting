256b01189c57eec949daadf307d3086fdccd215e
Sam Bokai <sam.bokai@scout24.com>
2018-06-03 20:23:48 +0200

    Implement filtering

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,6 @@ import { render } from 'react-dom';
 
 import App from './js/components/App';
 
+import './css/style.css';
+
 render(<App />, document.getElementById('app'));
--- src/js/components/Students/StudentTable.jsx
+++ src/js/components/Students/StudentTable.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import ReactDataGrid from 'react-data-grid';
+import { Toolbar } from 'react-data-grid-addons';
+
 import faker from 'faker';
 
 import LinkedList from '../../model/LinkedList';
@@ -19,23 +21,28 @@ class StudentTable extends Component {
           key: 'id',
           name: 'ID',
           sortable: true,
+          filterable: true,
         },
         {
           key: 'firstName',
           name: 'First Name',
           sortable: true,
+          filterable: true,
         },
         {
           key: 'lastName',
           name: 'Last Name',
           sortable: true,
+          filterable: true,
         },
         {
           key: 'universityMajor',
           name: 'Major',
           sortable: true,
+          filterable: true,
         },
       ],
+      filters: {},
     };
   }
 
@@ -43,21 +50,9 @@ class StudentTable extends Component {
     this.generateRandomStudents(this.props.randomStudentCount);
   }
 
-  generateRandomStudents(count) {
-    const randomStudents = new LinkedList();
-
-    for (let i = 0; i < count; i += 1) {
-      const randomMajor = () => {
-        const keys = Object.keys(Majors);
-        return Majors[keys[Math.floor(keys.length * Math.random())]];
-      };
-      const student = new Student(faker.name.firstName(), faker.name.lastName(), randomMajor());
-      randomStudents.append(student);
-    }
-
-    this.setState({ students: randomStudents });
-    return randomStudents;
-  }
+  onClearFilters = () => {
+    this.setState({ filters: {} });
+  };
 
   handleGridSort = (columnKey, direction) => {
     const compare = (a, b) => {
@@ -76,16 +71,64 @@ class StudentTable extends Component {
     }));
   };
 
-  rowGetter = i => this.state.students.get(i).value;
+  handleFilterChange = (filter) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        [filter.column.key]: filter.filterTerm,
+      },
+    });
+  };
+
+  generateRandomStudents(count) {
+    const randomStudents = new LinkedList();
+
+    for (let i = 0; i < count; i += 1) {
+      const randomMajor = () => {
+        const keys = Object.keys(Majors);
+        return Majors[keys[Math.floor(keys.length * Math.random())]];
+      };
+      const student = new Student(faker.name.firstName(), faker.name.lastName(), randomMajor());
+      randomStudents.append(student);
+    }
+
+    this.setState({ students: randomStudents });
+    return randomStudents;
+  }
+
+  filteredRows = () => {
+    const predicate = (student) => {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const key in this.state.filters) {
+        // eslint-disable-next-line no-prototype-builtins
+        if (this.state.filters.hasOwnProperty(key) && (student[key] === undefined || this.state.filters[key] !== '')) {
+          if (
+            (key !== 'id' && !student[key].toString().toLowerCase().includes(this.state.filters[key].toLowerCase()))
+              || (key === 'id' && student[key].toString().toLowerCase() !== this.state.filters[key].toLowerCase())
+          ) {
+            return false;
+          }
+        }
+      }
+      return true;
+    };
+
+    return this.state.students.filter(predicate);
+  };
+
+  rowGetter = i => this.filteredRows().get(i).value;
 
   render() {
     return (
       <ReactDataGrid
         columns={this.state.columns}
         rowGetter={this.rowGetter}
-        rowsCount={this.state.students.length}
+        rowsCount={this.filteredRows().length}
         minHeight={600}
         onGridSort={this.handleGridSort}
+        toolbar={<Toolbar enableFilter />}
+        onAddFilter={this.handleFilterChange}
+        onClearFilters={this.onClearFilters}
       />
     );
   }
